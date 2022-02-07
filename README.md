f40c3d433acccd531c28c953eced6d73a84b7e00
Alexander Rogov <rocketscientist@yandex.ru>
2022-02-07 22:33:08 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Part info in PAW menu
+
+Very small and simple mod for Kerbal Space Program.
+Mod works in VAB/SPH and adds some useful information about part to right-click menu (PAW menu):
+* part name (basically, part ID);
+* dry mass;
+* cost;
+* entry cost.
+
+By default, mod only applies itself to engines, but can be added to any parts.
+For engines, more information is displayed:
+* dry engine TWR (thrust / engine mass);
+* formatted string of additional data about engine (the same you can see in part information screen in left panel in VAB/SPH). If part has more than one ModuleEngines (for example, multi-mode engine like RAPIER), information for first two ModuleEngines will be displayed.
+
+Actually, one picture can tell more than a thousand words:
+![PartInfoInPAW](https://i.imgur.com/zEcSNql.png)
+
+
+
+## Dependencies
+
+* [Module manager (last version preferred)](https://github.com/sarbian/ModuleManager)
+
+
+## Supported KSP versions
+
+KSP 1.8.1 or newer is supported.
+
+
+## Licensing
+
+The MIT License (MIT)
+
+Copyright (c) 2022 Alexander Rogov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
---
+++ src/Modules/PartInfoInPAW.cs
@@ -0,0 +1,111 @@
+namespace PartInfoInPAW
+{
+	public class ModulePartInfoInPAW: PartModule
+	{
+		[KSPField(isPersistant = true)]
+		public string originalPartName = "";
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Name", groupName = "partInfo", groupDisplayName = "Part info")]
+		public string partName = "";
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Dry mass", groupName = "partInfo", groupDisplayName = "Part info", guiFormat = "F3", guiUnits = " t")]
+		public float partMass = 0.0f;
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Cost", groupName = "partInfo", groupDisplayName = "Part info")]
+		public float partCost = 0.0f;
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Entry cost", groupName = "partInfo", groupDisplayName = "Part info")]
+		public int partEntryCost = 0;
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Engine TWR", groupName = "partInfo", groupDisplayName = "Part info", guiFormat = "F2")]
+		public float partTWR = 0.0f;
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Info", groupName = "engine1Info", groupDisplayName = "Engine #1 info")]
+		public string engine1Info = "";
+
+		[KSPField(isPersistant = false, guiActiveEditor = true, guiActive = false, guiName = "Info", groupName = "engine2Info", groupDisplayName = "Engine #2 info")]
+		public string engine2Info = "";
+
+		protected ModuleEngines engine1;
+		protected ModuleEngines engine2;
+
+		protected bool InfoUpdated = false;
+		protected bool ShowTWR = true;
+
+		public void Update()
+		{
+			if (!InfoUpdated && HighLogic.LoadedSceneIsEditor)
+			{
+				UpdateInfo();
+			}
+		}
+
+		public void UpdateInfo()
+		{
+			ModuleEngines[] engines;
+			MultiModeEngine[] isMultimode;
+			float totalThrust = 0.0f;
+
+			if (originalPartName != "")
+			{
+				partName = originalPartName;
+			}
+			else
+			{
+				partName = part.partInfo.name;
+			}
+			partMass = part.mass;
+			partCost = part.partInfo.cost;
+			partEntryCost = part.partInfo.entryCost;
+
+			engines = part.GetComponents<ModuleEngines>();
+			if (engines.Length <= 0)
+			{
+				ShowTWR = false;
+				Fields["engine1Info"].guiActiveEditor = false;
+				Fields["engine2Info"].guiActiveEditor = false;
+			}
+			if (engines.Length > 0)
+			{
+				engine1 = engines[0];
+				totalThrust = engine1.GetMaxThrust();
+				engine1Info = "<br>" + engine1.GetInfo();
+				Fields["engine1Info"].guiActiveEditor = true;
+				if (engines.Length > 1)
+				{
+					engine2 = engines[1];
+					engine2Info = "<br>" + engine2.GetInfo();
+					Fields["engine2Info"].guiActiveEditor = true;
+					isMultimode = part.GetComponents<MultiModeEngine>();
+					if (isMultimode.Length <= 0)
+					{
+						totalThrust += engine2.GetMaxThrust();
+					}
+				}
+				else {
+					Fields["engine2Info"].guiActiveEditor = false;
+				}
+			}
+			if (ShowTWR)
+			{
+				partTWR = 0.0f;
+				if (partMass > 0)
+				{
+					partTWR = totalThrust / (partMass * 9.81f);
+				}
+				Fields["partTWR"].guiActiveEditor = true;
+			}
+			else
+			{
+				Fields["partTWR"].guiActiveEditor = false;
+			}
+			InfoUpdated = true;
+		}
+
+		[KSPEvent]
+        public void ModuleDataChanged(BaseEventDetails details)
+        {
+			InfoUpdated = false;
+        }
+	}
+}
